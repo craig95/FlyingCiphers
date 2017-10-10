15adfffe88e339e711a9c65bd3b61fce6bc294c3
DewaldDeJager <dewald.dj@hotmail.com>
2017-10-10 23:29:58 +0200

    Changed decode method and made shift_encode safer

--- ass4.py
+++ ass4.py
@@ -1,3 +1,8 @@
+"""
+COS 330 Assignment 4
+Craig van Heerden and Dewald de Jager
+"""
+
 import string
 import re
 from itertools import permutations
@@ -12,6 +17,7 @@ class DefaultDict(dict):
 
 class CeaserDecoder:
     def __init__(self, training_data):
+        self.all_shifts = []
         self.bi_counts = DefaultDict()
         self.tri_counts = DefaultDict()
         for word in training_data:
@@ -22,6 +28,11 @@ class CeaserDecoder:
             for trigram in trigrams(word):
                 self.tri_counts[trigram] += 1
 
+    def print_all_attempts(self):
+        self.all_shifts.sort(key=self.score)
+        for shift in self.all_shifts:
+            print(str(self.score(shift)) + '\t' + shift)
+
     def score(self, plaintext):
         bi_score = 1.0
         for bigram in bigrams(plaintext.lower()):
@@ -31,20 +42,59 @@ class CeaserDecoder:
         for trigram in trigrams(plaintext.lower()):
             tri_score += self.tri_counts[trigram]
 
-        final_score = bi_score + tri_score
-        if plaintext.lower()[:12] == "pleasebuyone":
-            print(plaintext)
-        return final_score
+        return bi_score + tri_score
+        # final_score = bi_score + tri_score
+        # if plaintext.lower()[:12] == "pleasebuyone":
+        #     print(plaintext)
+        # return final_score
 
     def decode(self, ciphertext):
-        all_shifts = []
-        for permutation in permutations([string.ascii_uppercase, '1234567890']):
-            permutation = ''.join(permutation)
-            all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+        has_lowercase = any(c.islower() for c in ciphertext)
+        has_uppercase = any(c.isupper() for c in ciphertext)
+        has_numbers = any(c.isdigit() for c in ciphertext)
+        self.all_shifts = []
+
+        if has_uppercase:
+            self.all_shifts += [shift_encode(ciphertext, n, string.ascii_uppercase) for n in
+                                range(len(string.ascii_uppercase))]
+
+        if has_lowercase:
+            self.all_shifts += [shift_encode(ciphertext, n, string.ascii_lowercase) for n in
+                                range(len(string.ascii_lowercase))]
+
+        if has_uppercase and has_numbers:
+            for permutation in permutations([string.ascii_uppercase, '1234567890']):
+                permutation = ''.join(permutation)
+                self.all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+            for permutation in permutations([string.ascii_uppercase, '0123456789']):
+                permutation = ''.join(permutation)
+                self.all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+
+        if has_lowercase and has_numbers:
+            for permutation in permutations([string.ascii_lowercase, '1234567890']):
+                permutation = ''.join(permutation)
+                self.all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+            for permutation in permutations([string.ascii_lowercase, '0123456789']):
+                permutation = ''.join(permutation)
+                self.all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+
+        if has_uppercase and has_lowercase:
+            for permutation in permutations([string.ascii_uppercase, string.ascii_lowercase]):
+                permutation = ''.join(permutation)
+                self.all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+
+        if has_uppercase and has_lowercase and has_numbers:
+            for permutation in permutations([string.ascii_uppercase, '1234567890', string.ascii_lowercase]):
+                permutation = ''.join(permutation)
+                self.all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+            for permutation in permutations([string.ascii_uppercase, '0123456789', string.ascii_lowercase]):
+                permutation = ''.join(permutation)
+                self.all_shifts += [shift_encode(ciphertext, n, permutation) for n in range(len(permutation))]
+
         # for i in range(len(all_shifts)-1):
         #     if not any(elem in all_shifts[i] for elem in '1234567890'):
         #         print(str(i) + ': ' + all_shifts[i])
-        return argmax(all_shifts, self.score)
+        return argmax(self.all_shifts, self.score)
 
 
 def bigrams(text):
@@ -56,10 +106,13 @@ def trigrams(text):
 
 
 def shift_encode(plaintext, n, alphabet):
-    "Encode text with a shift cipher that moves each letter up by n letters."
+    """Encode text with a shift cipher that moves each letter up by n letters."""
     code = ''
     for char in plaintext:
-        code += alphabet[(alphabet.index(char) + n) % 36]
+        try:
+            code += alphabet[(alphabet.index(char) + n) % len(alphabet)]
+        except ValueError:
+            pass
     return code
 
 
@@ -74,5 +127,8 @@ text = text.lower()
 words = text.split()
 
 test = CeaserDecoder(words)
-print(test.decode(
-    '2XQM5QN7A10QYUXXU104M0P59146T1R5TM4Q5M0PNQOM4QR7X01661NQ5QQ0NA6TQM7564MXUM0S18Q40YQ06M0P2XQM5QNQ8USUXM06'))
+answer = test.decode(
+    '2XQM5QN7A10QYUXXU104M0P59146T1R5TM4Q5M0PNQOM4QR7X01661NQ5QQ0NA6TQM7564MXUM0S18Q40YQ06M0P2XQM5QNQ8USUXM06')
+test.print_all_attempts()
+print('\nThe best plain text found was:\n')
+print(answer)
